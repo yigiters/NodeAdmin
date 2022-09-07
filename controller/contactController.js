68911db609c2c2eb8fc9677327d5c0da5bff0549
yigiters <salmanyigiter54@gmail.com>
2022-09-07 13:33:10 +0300

    NodeAdmin

--- controller/contactController.js
+++ controller/contactController.js
@@ -1,9 +1,45 @@
 const Contact = require('../model/contactModel')
 
 const contact_get = (req,res) => {
-    res.render('panel-contact', {auth: req.session.user})
+
+    Contact.findAll().then(result => {
+        res.render('panel-contact', {auth: req.session.user, data: result})
+    })
+
+}
+
+const contact_read = (req,res) => {
+    Contact.findOne({
+        where: {
+            id: req.query.id
+        }
+    }).then(result => {
+    
+        Contact.update({state: '1'}, {
+            where: {
+                id: req.query.id
+            }
+        })
+        if(result.state == 0) {
+            result1 = "Okunmadı"
+        } else {
+            result1 = "Okundu"
+        }
+        res.render('panel-contact-read', {auth: req.session.user, data: result, state: result1})
+    })
+}
+
+const contact_delete = (req,res) => {
+    Contact.destroy({
+        where: {
+            id: req.query.id
+        }
+    })
+    res.redirect('/contact-form')
 }
 
 module.exports = {
     contact_get,
+    contact_read,
+    contact_delete,
 }
--- routes/contactRoute.js
+++ routes/contactRoute.js
@@ -3,5 +3,7 @@ const router = express.Router()
 const contactController = require('../controller/contactController')
 
 router.get('/contact-form', contactController.contact_get)
+router.get('/contact/read', contactController.contact_read)
+router.get('/contact/delete', contactController.contact_delete)
 
 module.exports = router
